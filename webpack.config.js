8ceea45a8c0d559599564af69a1ac4d04776aa24
veereshmeesala <veereshmeesala@gmail.com>
2017-03-25 21:37:37 +0000

    folder structure and config file changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
+var path = require('path');
+
 module.exports = {
+    context: path.resolve('js'),
 	entry: ["./utils.js", "./app.js"],
 	output: {
+        path: path.resolve('build/js/'),
+        publicPath: "/public/assets/js/",
 		filename: "bundle.js"
 	},
-    watch: true
+    devServer: {
+        contentBase: "public"
+    },
+    module: {
+		loaders: [
+			{
+				test: /\.es6$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			}
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js']
+	}
 }
